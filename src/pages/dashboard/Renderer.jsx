af562e4c9cf5a406cc273e53073ef7de2645c2f6
yash khatri <ykhatri@integconsulting.com>
2025-08-17 08:44:02 -0400

    added vstack and button element

--- src/pages/dashboard/Renderer.jsx
+++ src/pages/dashboard/Renderer.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import { useDashboard } from "./DashboardContext";
-import { Box } from "@chakra-ui/react";
-import { InputEle, NumberEle, SelectEle } from "../../components/Common";
+import { Box, VStack } from "@chakra-ui/react";
+import {
+  ButtonEle,
+  InputEle,
+  NumberEle,
+  SelectEle,
+} from "../../components/Common";
 
 const ELEMENT_MAP = {
   input: InputEle,
   number: NumberEle,
   select: SelectEle,
+  button: ButtonEle,
 };
 
 const Renderer = () => {
@@ -14,19 +20,25 @@ const Renderer = () => {
 
   const [formValues, setFormValues] = useState({});
   return (
-    <Box flex="1" maxW="50%">
-      {JSON.stringify(fieldConfig)}---
-      {JSON.stringify(formValues)}---
-      {fieldConfig.map(({ name, type }) => {
-        const Comp = ELEMENT_MAP[type];
-        return (
-          <Comp
-            name={name}
-            value={formValues[name]}
-            onChange={(n, v) => setFormValues((prev) => ({ ...prev, [n]: v }))}
-          />
-        );
-      })}
+    <Box flex="1" maxW="100%">
+      {/* {JSON.stringify(fieldConfig)}---
+      {JSON.stringify(formValues)}--- */}
+      <VStack spacing={4} align="stretch">
+        {fieldConfig.map(({ name, type }) => {
+          const Comp = ELEMENT_MAP[type];
+          return (
+            <Comp
+              name={name}
+              value={formValues[name]}
+              allFormValues={formValues}
+              onChange={(n, v) =>
+                setFormValues((prev) => ({ ...prev, [n]: v }))
+              }
+              onReset={() => setFormValues({})}
+            />
+          );
+        })}
+      </VStack>
     </Box>
   );
 };
